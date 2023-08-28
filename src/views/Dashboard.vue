080bab343bb3190473291a19a04c36f9b0f89380
paurush rai <paurushrai99@gmail.com>
2023-08-29 00:56:18 +0530

    Update Dashboard.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -15,7 +15,7 @@
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
           <div class="mx-auto max-w-2xl lg:text-center">
             <h2 class="text-base font-semibold leading-7 text-indigo-600">Web Application</h2>
-            <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus corporis nulla animi. Voluptatibus eligendi, illo error incidunt similique vel sit tempore ducimus doloremque harum in magni minus sunt explicabo nemo!</p>
+            <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus corporis nulla animi.</p>
             <p class="mt-6 text-lg leading-8 text-gray-600">Quis tellus eget adipiscing convallis sit sit eget aliquet
               quis. Suspendisse eget egestas a elementum pulvinar et feugiat blandit at. In mi viverra elit nunc.</p>
           </div>
