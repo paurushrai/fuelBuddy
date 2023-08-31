91ee36ee3ca5dea8d3fc37481faaed9c120dff6f
paurush rai <paurushrai99@gmail.com>
2023-08-31 14:50:09 +0530

    .

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router/auto'
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import Home from "../views/Home.vue";
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
 
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VueRouter from 'unplugin-vue-router/vite'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    VueRouter({
+      /* options */
+    }),
     vue(),
   ],
   resolve: {
